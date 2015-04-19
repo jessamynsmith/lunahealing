ff065f184f71455c185c00ed740b4479ca538880
Jessamyn Smith <geekchick77@gmail.com>
2015-04-19 19:38:55 -0400

    Removed extraneous subdir

--- lunahealing/settings/common.py
+++ lunahealing/settings/common.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '/lunahealing/static/',
+    BASE_DIR + '/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -45,7 +45,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '/lunahealing/templates/',
+    BASE_DIR + '/templates/',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
