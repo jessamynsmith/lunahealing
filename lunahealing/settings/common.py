2e9f394b7d39fac0454aa85ee193698df6f87eef
Jessamyn Smith <geekchick77@gmail.com>
2015-04-19 19:30:09 -0400

    Removed duplicate setting, fixed reference to static in settings.

--- lunahealing/settings/common.py
+++ lunahealing/settings/common.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '/static/',
+    BASE_DIR + '/lunahealing/static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -74,13 +74,6 @@ ROOT_URLCONF = 'lunahealing.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'lunahealing.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '/templates/',
-)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
