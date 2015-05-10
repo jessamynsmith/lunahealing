dd157d09b478d8a1d18eaa5a9f0e9fd142a50205
Jessamyn Smith <geekchick77@gmail.com>
2015-05-10 12:40:10 -0400

    Improved deployment script

--- scripts/deploy_to_heroku.sh
+++ scripts/deploy_to_heroku.sh
@@ -6,18 +6,20 @@ set -e
 CIRCLE=$1
 
 DEPLOY_DATE=`date "+%FT%T%z"`
+SECRET=$(openssl rand -base64 58 | tr '\n' '_')
 
 heroku config:set --app=lunahealing \
 NEW_RELIC_APP_NAME='lunahealing' \
 ADMIN_EMAIL="jessamyn@lunahealing.ca" \
 ADMIN_NAME="lunahealing" \
 DJANGO_SETTINGS_MODULE=lunahealing.settings.production \
-DJANGO_SECRET_KEY=$DJANGO_SECRET_KEY \
+DJANGO_SECRET_KEY="$SECRET" \
 DEPLOY_DATE="$DEPLOY_DATE" \
 > /dev/null
 
 if [ $CIRCLE ]
 then
+    git fetch origin --unshallow
     git push git@heroku.com:lunahealing.git $CIRCLE_SHA1:refs/heads/master
 else
     git push heroku master
